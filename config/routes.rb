c0bd31757d69c795f04b940ec8924354e8aecca1
Brandi Campbell <brandic@mwwondemand.com>
2019-06-28 15:10:46 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :plants
 	namespace :api do
 		namespace :v1 do
 			resources :items
-  		resources :lists
+			resources :lists
+			resources :plants
 		end
 	end
 	
